1b9acb4cd2ecfee11e49d96f2b590746429e76a4
YK <mountan_spirit-github@yahoo.de>
2018-03-08 09:19:26 +0100

    Solution matrix array implementation: Implemented plain array copy way of satisfying solution matrix interface. Undo is implemented by slow array copy.

---
+++ src/ArrayMatrix.java
@@ -0,0 +1,88 @@
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class ArrayMatrix implements SolutionMatrix {
+    private final Deque<char[][]> history;
+    private char[][] currentState;
+
+    public ArrayMatrix(int initialRowCount, int initialColCount) {
+        history = new LinkedList<>();
+        currentState = new char[initialRowCount][initialColCount];
+    }
+
+    @Override
+    public char get(int row, int col) {
+        return currentState[row][col];
+    }
+
+    @Override
+    public void removeRow(int row) {
+        if (row >= currentState.length || row < 0) {
+            throw new ArrayIndexOutOfBoundsException(row);
+        }
+
+        history.push(currentState);
+
+        char[][] newMatrix = new char[getRowCount() - 1][];
+        int offset;
+        for (int i = 0; i < newMatrix.length; ++i) {
+            if (i >= row) {
+                offset = 1;
+            } else {
+                offset = 0;
+            }
+
+            newMatrix[i] = Arrays.copyOf(currentState[i + offset], currentState[i + offset].length);
+        }
+
+        currentState = newMatrix;
+    }
+
+    @Override
+    public void removeColumn(int col) {
+        if (col >= currentState.length || col < 0) {
+            throw new ArrayIndexOutOfBoundsException(col);
+        }
+
+        history.push(currentState);
+
+        char[][] newMatrix = new char[getRowCount()][getColumnCount() - 1];
+        int offset;
+        for (int i = 0; i < newMatrix.length; ++i) {
+            for (int j = 0; j < newMatrix[i].length; ++j) {
+                if (j >= col) {
+                    offset = 1;
+                } else {
+                    offset = 0;
+                }
+
+                newMatrix[i][j] = currentState[i][j + offset];
+            }
+        }
+
+        currentState = newMatrix;
+    }
+
+    @Override
+    public void undo() {
+        if (canUndo()) {
+            currentState = history.pop();
+        }
+    }
+
+    @Override
+    public boolean canUndo() {
+        return !history.isEmpty();
+    }
+
+    @Override
+    public int getRowCount() {
+        return currentState.length;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return currentState.length > 0 ? currentState[0].length : 0;
+    }
+}
